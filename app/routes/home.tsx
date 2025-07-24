73323a42ae28f074e3792967a3a99928f577515b
itsKunalChaudhary <kunal59chaudhary@gmail.com>
2025-07-24 15:19:29 +0530

    Added Resume Upload Form and Created Drag and Drop for File Upload

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -27,14 +27,12 @@ export default function Home() {
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check  AI-powered feedback.</h2>
       </div>
-
       {resumes.length > 0 && (
           <div className={"resumes-section py-10"}>
             {resumes.map((resume) => (
                 <ResumeCard key={resume.id} resume={resume} />
             ))}
           </div>
-
       )}
     </section>
   </main>;
